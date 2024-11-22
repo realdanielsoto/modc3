80be95a69d455bc809f4d8a9604d615cbdd151f9
Daniel Soto <DanielxSoto17@gmail.com>
2024-11-22 18:52:06 -0500

    Implemented functions for sorting, average salary, and random employee selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,67 @@
 // Get a reference to the #add-employees-btn element
-const addEmployeesBtn = document.querySelector('#add-employees-btn');
+const addEmployeesBtn = document.querySelector("#add-employees-btn");
 
 // Collect employee data
 const collectEmployees = function() {
+  const employees = [];
+  let continueAdding = true;
+
+  while (continueAdding) {
+    const firstName = prompt("Enter employee's first name:");
+    const lastName = prompt("Enter employee's first name:");
+    const salary = parseFloat(prompt("Enter employee's salary:"));
+  
+    if (firstName && lastName && !isNaN(salary)) {
+      employees.push({ firstName, lastName, salary });
+    } else {
+      alert("Invalid input. Please try again.")
+    }
+
+    continueAdding = confirm("Would you like to add another employee?");
+  }
+
+  return employees;
+  
+};
+
   // TODO: Get user input to create and return an array of employee objects
-}
+
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
+  if (employeesArray.length === 0) {
+    console.log("No employees to calculate salary.");
+    return;
+  }
+
+  const totalSalary = employeesArray.reduce((sum, employee) => sum + employee.salary, 0);
+  const averageSalary = totalSalary / employeesArray.length;
+
+  console.log(`The average salary is $${averageSalary.toFixed(2)}`);
+};
+
+const sortEmployeesByLastName = function (employeesArray) {
+  return employeesArray.sort((a,b) => a.lastName.localCompare(b.lastName));
+
+};
+
+
   // TODO: Calculate and display the average salary
-}
+
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
-  // TODO: Select and display a random employee
-}
+  if (employeesArray.length === 0) {
+    console.log("No employees available for selection.");
+    return;
+  }
 
+  const randomIndex = Math.floor(Math.random() * employeesArray.length);
+  console.log("Random Employee:", employeesArray[randomIndex]);
+};
+
+
+// TODO: Select and display a random employee
 /*
   ====================
   STARTER CODE
